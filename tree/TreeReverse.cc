2fd242cda3a19c5c5bf7364fb373d65c64317e98
mzahng160 <mzhang160@163.com>
2019-08-26 20:01:43 +0800

    add tree delete

--- tree/TreeReverse.cc
+++ tree/TreeReverse.cc
@@ -63,8 +63,31 @@ void midOrderTranverse(BiTree tree)
 	midOrderTranverse(tree->rchild);
 }
 
-void midOrder(BiTree tree)
+void midOrder(BiTree pRoot)
 {
+	if (pRoot == NULL)
+		return;
+
+	BiTree p = pRoot;
+	stack<BiTree> s;
+
+	while (p != NULL || !s.empty())
+	{
+		while (p != NULL)
+		{
+			s.push(p);
+			
+			p = p->lchild;
+		}
+
+		if (!s.empty())
+		{
+			p = s.top();
+			cout << p->data << " ";
+			s.pop();
+			p = p->rchild;
+		}
+	}
 }
 
 void PostOrderTraverse(BiTree root)
@@ -109,13 +132,17 @@ void PostOrder(BiTree pRoot)
 	}
 }
 
-int levelOrderTraveral(BiTree pRoot)
+/*
+ * this function has not been written finsih, cannot print
+ * all data in tree, it will be finish later.
+ */
+void printTree(BiTree pRoot, int dataPos, int len)
 {
 	queue<BiTree> q;
 	BiTree front;
 
 	if (pRoot == NULL)
-		return 0;
+		return ;
 
 	q.push(pRoot);
 
@@ -123,6 +150,99 @@ int levelOrderTraveral(BiTree pRoot)
 	int toBePrinted = 1;
 	int level = -1;
 	int sum = 0;
+	int dt = 0;
+	for (int i = 0; i < len/2; i++)
+	{
+		cout << "*";
+	}
+
+	while (!q.empty())
+	{
+		front = q.front();
+		q.pop();
+
+		if (front->lchild)
+		{
+			q.push(front->lchild);
+			nextLevel++;
+		}
+
+
+		if (front->rchild)
+		{
+			q.push(front->rchild);
+			nextLevel++;
+		}
+
+		cout << front->data;
+
+		toBePrinted--;
+
+		if (toBePrinted == 0)
+		{
+			sum = 2 << (level+1);
+			dt = (len + 1) / sum;
+
+			cout << endl;
+			toBePrinted = nextLevel;
+			nextLevel = 0;
+
+			++level;
+		}
+
+	}
+}
+
+string getSpace(int num)
+{
+	string buf;
+	for (int i = 0; i < num; i++)
+	{
+		buf.append(" ");
+	}
+
+	return buf;
+}
+
+void printInOrder(BiTree pRoot, int height, string to, int len)
+{
+	if (pRoot == NULL)
+		return;
+
+	printInOrder(pRoot->rchild, height + 1, "v", len);
+	string val;
+	val.append(to);
+	val.append(std::to_string(pRoot->data).c_str());
+	val.append(to);
+
+	int lenM = val.length();
+	int lenL = (len - lenM) / 2;
+	int lenR = len - lenM - lenL;
+	val = getSpace(lenL) + val + getSpace(lenR);
+	cout << getSpace(height*len) + val << endl;
+	printInOrder(pRoot->lchild, height + 1, "^", len);
+}
+
+void printTree_2(BiTree pRoot)
+{
+	cout << "Binary tree" << endl;
+	printInOrder(pRoot, 0, "H", 17);
+	cout << endl;
+}
+
+void levelOrderTraveral(BiTree pRoot)
+{
+	queue<BiTree> q;
+	BiTree front;
+
+	if (pRoot == NULL)
+		return ;
+
+	q.push(pRoot);
+
+	int nextLevel = 0;
+	int toBePrinted = 1;
+	int level = -1;
 
 	while (!q.empty())
 	{
@@ -146,17 +266,85 @@ int levelOrderTraveral(BiTree pRoot)
 		toBePrinted--;
 		if (toBePrinted == 0)
 		{
-			sum += 2 << level;			 
-
 			cout << endl;
 			toBePrinted = nextLevel;
 			nextLevel = 0;			
 
 			++level;
 		}		
-	}
+	}	
+}
 
-	return sum+1;
+ElemType GetMinNode(BiTree pRoot)
+{
+	if (pRoot)
+	{
+		while (pRoot&&pRoot->lchild != NULL)
+		{
+			pRoot = pRoot->lchild;
+		}
+		return pRoot->data;
+	}
+}
+
+ElemType GetMaxNode(BiTree pRoot)
+{
+	if (pRoot)
+	{
+		while (pRoot&&pRoot->rchild != NULL)
+		{
+			pRoot = pRoot->rchild;
+		}
+		return pRoot->data;
+	}
+}
+
+bool Search(BiTree pRoot, ElemType key)
+{
+	if (pRoot == NULL)
+		return false;
+	if (key < pRoot->data)
+		return Search(pRoot->lchild, key);
+	else if (key > pRoot->data)
+		return Search(pRoot->rchild, key);
+	else
+		return true;
+}
+
+void removeNode(BiTree& pRoot, ElemType key)
+{
+	
+	if (pRoot == NULL)
+		return;
+	if (key < pRoot->data)
+	{
+		removeNode(pRoot->lchild, key);
+	}
+	else if (key > pRoot->data)
+	{
+		removeNode(pRoot->rchild, key);
+	}
+	else 
+	{
+		if (pRoot->rchild == NULL && pRoot->lchild == NULL)
+		{
+			cout << "****"<< pRoot->data << endl;
+			pRoot = NULL;
+			return;
+		}
+		else if (pRoot->lchild == NULL)
+		{			
+			pRoot = pRoot->rchild;
+		}
+		else if (pRoot->rchild == NULL)
+		{
+			pRoot = pRoot->lchild;
+		}
+
+		pRoot->data = GetMinNode(pRoot);
+
+		removeNode(pRoot->rchild, pRoot->data);
+	}
 }
 
 int getHeight(BiTree root) {
@@ -167,7 +355,6 @@ int getHeight(BiTree root) {
 	int rightheight = getHeight(root->rchild);
 	return leftheight > rightheight ? (leftheight + 1):(rightheight + 1);
 }
-
 
 void SearchTreeNode(BiTree& root, BiTree& a)
 {
@@ -219,6 +406,8 @@ void CreateOrderBinaryTree(BiTree& tree, int* a, int len)
 
 int main()
 {
+
+	cout << sizeof(long) << " " << sizeof(int) << " " << sizeof(char) << " " << sizeof(double) << " " << sizeof(float) << endl;
 	
 	//int a[] = { 62, 88, 58, 47, 35, 73, 51, 99, 37, 93, 23, 27, 45, 21, 12 };
 	//int a[] = { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'M' };
@@ -229,15 +418,66 @@ int main()
 	int len = sizeof(a)/sizeof(a[0]);
 	CreateOrderBinaryTree(tree, a, len);
 
-	cout << "tree heigh " << getHeight(tree) << endl;
+	int treeHeight = getHeight(tree);
+	cout << "tree heigh " << treeHeight << endl;	
+#if 0
+	int sum = 0;
+	for (int i = 0; i < treeHeight-1; i++)
+		sum += 2 << i;
+	sum += 1;
+	
+	printTree(tree, sum/2, sum);
+	cout << endl;
+#endif
 
-	cout << len << "levelOrderTraveral" << endl;
-	cout << levelOrderTraveral(tree) << endl;
+	cout << "levelOrderTraveral" << endl;
+	levelOrderTraveral(tree);
+	cout << endl;
 
+	/******************************/
+	cout << "PreOrderTraverse" << endl;
+	PreOrderTraverse(tree);
+	cout << endl;
 
+	cout << "PreOrder" << endl;
+	PreOrder(tree);
+	cout << endl;
+
+	/******************************/
 	cout <<  "PostOrderTraverse" << endl;
 	PostOrderTraverse(tree);
+	cout << endl;
+
 	cout << "PostOrder" << endl;
 	PostOrder(tree);
-	
+	cout << endl;
+
+	/******************************/
+	cout << "midOrderTranverse" << endl;
+	midOrderTranverse(tree);
+	cout << endl;
+
+	cout << "midOrder" << endl;
+	midOrder(tree);	
+	cout << endl;
+
+	/******************************/
+	cout << "min data" << endl;
+	cout << GetMinNode(tree) << endl;
+
+	cout << "max data" << endl;
+	cout << GetMaxNode(tree) << endl;
+
+	/******************************/	
+	cout << "max data" << endl;
+	cout << (Search(tree, a[8]) ? "true":"false") << endl;
+
+
+	/******************************/
+	cout << "delete" << endl;
+	removeNode(tree, a[5]);
+	levelOrderTraveral(tree);	
+	cout << endl;
+
+	printTree_2(tree);
 }
